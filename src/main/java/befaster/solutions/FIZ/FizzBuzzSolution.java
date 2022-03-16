d48cedd9cd72d02c55e36c47f0126d5e69c5cb77
sbx_user1051 <sbx_user1051@169.254.160.145>
2022-03-16 21:25:57 +0000

    Wed Mar 16 21:25:57 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,9 +3,20 @@ package befaster.solutions.FIZ;
 import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
+    private static final String threeMultiple = "fizz";
+    private static final String fiveMultiple = "buzz";
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+
+        if(number % 3 == 0 && number % 5 == 0)
+            return threeMultiple + " " + fiveMultiple;
+        if(number % 3 == 0)
+            return threeMultiple;
+        if(number % 5 == 0)
+            return fiveMultiple;
+
+        return number.toString();
     }
 
 }
+
